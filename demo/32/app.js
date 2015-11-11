81d03e6f22b1fad71e7e8bfcccf41f0bd30ee146
YIXUNFE <jianchen@yixun.com>
2015-11-11 15:21:08 +0800

    组件通信方式变更

--- demo/32/app.js
+++ demo/32/app.js
@@ -1,12 +1,21 @@
 var ClickBtn = React.createClass({
   getInitialState: function() {
-    return {klass: 'btn'}
+    return {klass: 'btn', status: 'off'}
+  },
+  onClick: function () {
+    this.setState({status: this.state.status === 'off' ? 'on' : 'off'})
   },
   render: function () {
-    return <div ref="btn" className={this.state.klass + ' ' + this.props.status} >Click Me</div>
+    return <div ref="btn" className={this.state.klass + ' ' + this.state.status} >Click Me</div>
   },
   getStatus: function () {
     return this.state.status
+  },
+  on: function () {
+    this.setState({status: 'on'})
+  },
+  off: function () {
+    this.setState({status: 'off'})
   }
 })
 
@@ -16,7 +25,7 @@ var Popup = React.createClass({
     var self = this
     dom.addEventListener('mousemove', function (e) {self.onMouseMove(e)})
     dom.addEventListener('mouseup', function (e) {self.onMouseUp(e)})
-    return {left: 0, top: 0, startLeft: 0, startTop: 0, x: 0, y: 0, ready: false}
+    return {status: 'none', left: 0, top: 0, startLeft: 0, startTop: 0, x: 0, y: 0, ready: false}
   },
   onMouseDown: function (e) {
     var x = e.clientX,
@@ -44,11 +53,17 @@ var Popup = React.createClass({
     var style = {
       left: this.state.left,
       top: this.state.top,
-      display: this.props.status
+      display: this.state.status
     }
     return (
       <div ref="popup" onMouseDown={this.onMouseDown} className="popup" style={style} >Drag Me</div>
     )
+  },
+  show: function () {
+    this.setState({status: 'block'})
+  },
+  hide: function () {
+    this.setState({status: 'none'})
   }
 })
 
@@ -56,21 +71,31 @@ var Component = React.createClass({
   getInitialState: function() {
     return {status: 'off'}
   },
-  onClick: function (e) {
-    this.setState({status: this.state.status === 'off' ? 'on' : 'off'})
+  onClick: function () {
+    this.toggle()
+    
   },
   render: function () {
     return (
       <div className="component">
         <div onClick={this.onClick} >
-          <ClickBtn status={this.state.status} />
+          <ClickBtn ref="btn" />
         </div>
-        <Popup status={this.state.status === 'off' ? 'none' : 'block'} />
+        <Popup ref="pop" />
       </div>
     )
   },
   toggle: function () {
-    this.setState({status: this.state.status === 'off' ? 'on' : 'off'})
+    var refs = this.refs
+    this.setState({status: this.state.status === 'off' ? 'on' : 'off'}, function () {
+      if (this.state.status === 'on') {
+        refs.btn.on()
+        refs.pop.show()
+      } else {
+        refs.btn.off()
+        refs.pop.hide()
+      }
+    })
   }
 })
 
